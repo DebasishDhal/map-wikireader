fbf7aed5515b990d7720ebe5e7b023df65484d52
DebasishDhal <debasishdhal99ddhal@gmail.com>
2025-05-30 18:31:54 +0530

    Basic map setup on frontend

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MapContainer, TileLayer, 
+        // Marker, 
+        // Popup 
+    } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+const Map = () => {
+    return (
+        <MapContainer
+            center={[0, 0]}
+            zoom={2}
+            style={{ height: '100vh', width: '100%' }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
+    );
+};
+
+export default Map;
