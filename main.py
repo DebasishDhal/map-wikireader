a6ad668a5fe706ff278e3c5d3281bcf3493efefd
DebasishDhal <debasishdhal99ddhal@gmail.com>
2025-06-11 15:05:59 +0530

    Add basic wikipage renderer
    It's rendering the text, tables but it's not rendering the images yet, because the image links point to an external server, while my system think it's an internal link [that's what I think].

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 # from pydantic import BaseModel
 import requests
 from geopy.geocoders import Nominatim
